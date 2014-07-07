004a3b12ce345b18108b115400f4184f47539c72
reus <reusee@gmail.com>
2014-07-07 19:39:36 +0800

    add download

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -25,6 +27,7 @@ func main() {
 	var listIds []string
 	var printJson bool
 	var random bool
+	var download bool
 	for _, arg := range os.Args[1:] {
 		if regexp.MustCompile(`^[0-9]+$`).MatchString(arg) {
 			listIds = append(listIds, arg)
@@ -32,6 +35,8 @@ func main() {
 			printJson = true
 		} else if arg == "random" {
 			random = true
+		} else if arg == "download" {
+			download = true
 		} else if arg == "nao" {
 			listIds = []string{"20219787"}
 		} else if arg == "anisong" {
@@ -49,9 +54,11 @@ func main() {
 		}
 		Mp3 struct {
 			DfsId uint64
+			Size  int64
 		} `json:"bMusic"`
+		Url string
 	}
-	var songs []Song
+	var songs []*Song
 	var lock sync.Mutex
 	getSongs := func(listId string) {
 		// get list json
@@ -81,7 +88,7 @@ func main() {
 		// decode
 		var result struct {
 			Result struct {
-				Tracks []Song
+				Tracks []*Song
 				Name   string
 			}
 			Code int
@@ -120,7 +127,7 @@ func main() {
 		}
 	}
 
-	// play
+	// get url
 	enc := func(id uint64) string {
 		b1 := []byte("3go8&$8*3*3h0k(2)2")
 		b2 := []byte(fmt.Sprintf("%d", id))
@@ -136,13 +143,63 @@ func main() {
 		return res
 	}
 	for _, song := range songs {
-		p("%s", song.Name)
-		for _, artist := range song.Artists {
-			p(" %s", artist.Name)
+		song.Url = fmt.Sprintf("http://m2.music.126.net/%s/%d.mp3", enc(song.Mp3.DfsId), song.Mp3.DfsId)
+		//p("%s\n", song.Name)
+	}
+
+	if download {
+		wg := new(sync.WaitGroup)
+		wg.Add(len(songs))
+		sem := make(chan bool, 8)
+		for _, song := range songs {
+			sem <- true
+			go func(song *Song) {
+				var artistNames []string
+				for _, artist := range song.Artists {
+					artistNames = append(artistNames, artist.Name)
+				}
+				filename := fmt.Sprintf("%s - %s.mp3", strings.Join(artistNames, " "), song.Name)
+				// check exists
+				var exists bool
+				stat, err := os.Stat(filename)
+				if err == nil && stat.Size() == song.Mp3.Size {
+					exists = true
+				}
+				if exists {
+					p("skip %s\n", filename)
+				} else { // download
+					p("download %s.\n", filename)
+					resp, err := http.Get(song.Url)
+					if err != nil {
+						log.Fatal("download %s %v", song.Url, err)
+					}
+					file, err := os.Create(filename)
+					if err != nil {
+						log.Fatal("create %s %v", filename, err)
+					}
+					_, err = io.Copy(file, resp.Body)
+					if err != nil {
+						log.Fatal("copy %s %v", filename, err)
+					}
+					file.Close()
+					resp.Body.Close()
+				}
+
+				<-sem
+				wg.Done()
+			}(song)
 		}
-		url := fmt.Sprintf("http://m2.music.126.net/%s/%d.mp3", enc(song.Mp3.DfsId), song.Mp3.DfsId)
-		p(" http://music.163.com/#/song?id=%d", song.Id)
-		p("\n")
-		exec.Command("mpg123", url).Run()
+		wg.Wait()
+	} else {
+		// play
+		for _, song := range songs {
+			p("%s", song.Name)
+			for _, artist := range song.Artists {
+				p(" %s", artist.Name)
+			}
+			p(" http://music.163.com/#/song?id=%d", song.Id)
+			p("\n")
+			exec.Command("mpg123", song.Url).Run()
+		}
 	}
 }
